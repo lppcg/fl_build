38feee7af015c15c8066772fe2b3d7b67ea76ff8
lollipopkit <lollipopkit@qq.com>
2024-05-25 23:07:49 +0800

    new: add ver to file name

--- lib/make.dart
+++ lib/make.dart
@@ -45,9 +45,9 @@ Future<void> flutterBuildAndroid() async {
 
   // {originName: newName}
   final namesMap = {
-    'app-arm64-v8a-release.apk': '${appName}_arm64.apk',
-    'app-armeabi-v7a-release.apk': '${appName}_arm.apk',
-    'app-x86_64-release.apk': '${appName}_amd64.apk',
+    'app-arm64-v8a-release.apk': '${appName}_${COMMIT_COUNT}_arm64.apk',
+    'app-armeabi-v7a-release.apk': '${appName}_${COMMIT_COUNT}_arm.apk',
+    'app-x86_64-release.apk': '${appName}_${COMMIT_COUNT}_amd64.apk',
   };
   for (final entry in namesMap.entries) {
     final origin = entry.key;
@@ -112,12 +112,12 @@ Future<void> flutterBuildLinux() async {
     print(appimg.stderr);
     exit(appimg.exitCode);
   }
-  await File('$appName-x86_64.AppImage').rename('${appName}_amd64.AppImage');
+  await File('$appName-x86_64.AppImage').rename('${appName}_${COMMIT_COUNT}_amd64.AppImage');
 }
 
 Future<void> flutterBuildWin() async {
   await flutterBuild('windows');
-  // TODO: Zip build output dir to ./${appName}_amd64.zip
+  // TODO: Zip build output dir to ./${appName}_${COMMIT_COUNT}_amd64.zip
 }
 
 Future<void> changeAppleVersion() async {
