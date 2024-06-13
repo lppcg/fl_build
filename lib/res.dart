b7e3698aa0ad2fcf0629d140b134276b9e42dac0
lollipopkit <10864310+lollipopkit@users.noreply.github.com>
2024-06-13 23:30:02 +0800

    fix
    Signed-off-by: lollipopkit <10864310+lollipopkit@users.noreply.github.com>

--- lib/res.dart
+++ lib/res.dart
@@ -6,7 +6,7 @@ import 'dart:io';
 final COMMIT_COUNT = () {
   final result = Process.runSync('git', ['rev-list', '--count', 'HEAD']);
   final val = int.tryParse(result.stdout.toString().trim()) ?? 0;
-  if (envFile == null) return val;
+  if (envFile != null) return val;
   // - Before pushing, ver = 1.
   // - After pushing, ver = 2, but the version wrote in remote file is still 1.
   // So, we need to increment the version by 1 to correctly match the version.
